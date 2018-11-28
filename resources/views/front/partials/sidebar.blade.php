43129a07092eacd1011b2eb8e8e39d033ce5eb10
Wataru Nakayama <nakayama@marinax.co.jp>
2018-11-28 23:15:22 +0700

    レイアウトの全体調整、ダミーテキスト埋め込み

--- resources/views/front/partials/sidebar.blade.php
+++ resources/views/front/partials/sidebar.blade.php
@@ -1,54 +1,37 @@
 <!-- Sidebar  -->
 <nav class="sidebar">
     <div class="sidebar-header">
-        <h3>Bootstrap Sidebar</h3>
+        <a href="/"><h3 class="text-center">{{config('app.name')}}</h3></a>
     </div>
     <ul class="list-unstyled components">
-        <p>Dummy Heading</p>
+        <p>{{__('sidebar.title')}}</p>
         <li class="active">
-            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">{{__('sidebar.home')}}</a>
             <ul class="collapse list-unstyled" id="homeSubmenu">
                 <li>
-                    <a href="#">Home 1</a>
+                    <a href="#">{{__('sidebar.home1')}}</a>
                 </li>
                 <li>
-                    <a href="#">Home 2</a>
+                    <a href="#">{{__('sidebar.home2')}}</a>
                 </li>
                 <li>
-                    <a href="#">Home 3</a>
+                    <a href="#">{{__('sidebar.home3')}}</a>
                 </li>
             </ul>
         </li>
         <li>
-            <a href="#">About</a>
+            <a href="#">{{__('sidebar.about')}}</a>
         </li>
         <li>
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-            <ul class="collapse list-unstyled" id="pageSubmenu">
-                <li>
-                    <a href="#">Page 1</a>
-                </li>
-                <li>
-                    <a href="#">Page 2</a>
-                </li>
-                <li>
-                    <a href="#">Page 3</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a href="#">Portfolio</a>
-        </li>
-        <li>
-            <a href="#">Contact</a>
+            <a href="/picks">{{__('sidebar.picks')}}</a>
         </li>
     </ul>
     <ul class="list-unstyled CTAs">
         <li>
-            <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+            <a href="#" class="download mb-3">{{__('global.cta')}}</a>
         </li>
         <li>
-            <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+            <a href="#" class="article">{{__('global.back')}}</a>
         </li>
     </ul>
 </nav>
