ccd47e83d769b85fb08a00e09adc4e227d4dd183
Wataru Nakayama <nakayama@marinax.co.jp>
2018-11-28 23:14:33 +0700

    サンプル用の言語ファイルを追加

---
+++ resources/lang/en/sidebar.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Siderbar Title',
+    'home' => 'HOME',
+    'home1' => 'HOME1',
+    'home2' => 'HOME2',
+    'home3' => 'HOME2',
+    'about' => 'ABOUT',
+    'picks' => 'PICKS',
+];
