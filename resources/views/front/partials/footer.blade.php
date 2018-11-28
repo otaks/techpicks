e3ba7184d0bad0d13fdbbeb124f8e1d9e495be3a
Wataru Nakayama <nakayama@marinax.co.jp>
2018-11-28 23:15:22 +0700

    レイアウトの全体調整、ダミーテキスト埋め込み

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -1,6 +1,6 @@
 
 <footer class="footer">
-    <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+    <div class="container-fluid">
+        <p class="text-muted text-center mt-3">{{__('footer.title')}}</p>
     </div>
 </footer>
