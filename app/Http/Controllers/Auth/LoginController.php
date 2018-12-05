540efa9c046768ee4d09d9e4b13c116be4b235dc
鴛海直輝 <igbmh.yz.way2@gmail.com>
2018-12-05 20:58:12 +0900

    Turning direction

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Socialite;
+use App\User;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -35,5 +39,37 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function socialLogin()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function callback()
+    {
+        // ソーシャルサービス（情報）を取得
+        $userSocial = $data = Socialite::driver('facebook')->user();
+
+        //e mailで登録を調べる
+        $user = User::where(['email' => $userSocial->getEmail()])->first();
+
+
+        // 登録（email）の有無で分岐
+        if ($user) {
+            //す でに登録済みの場合は、そのままログイン
+            Auth::login($user);
+            return redirect('/');
+        } else {
+            // 初めての方は登録を
+            $newUser = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+            ]);
+
+            //そのままログイン
+            Auth::login($newUser);
+            return redirect('/');
+        }
     }
 }
