4705f79f7cf0e274d4792955cc780947a74f9008
HiromichiYamamoto <gamon.co2@gmail.com>
2018-12-16 15:31:02 +0900

    コンフリクト修正

--- app/Http/Controllers/Front/PostController.php
+++ app/Http/Controllers/Front/PostController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers\Front;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
+use App\Services\PostService;
+use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
+    private $prefix = 'front.posts.';
 
-    private $prefix = 'front.post.';
+    private $postService;
+
+    public function __construct(PostService $postService)
+    {
+        $this->postService = $postService;
+    }
 
     public function create()
     {
         return view("{$this->prefix}create");
     }
 
+    public function store(PostRequest $request)
+    {
+        $posts = $this->postService->save($request->all());
+        return redirect('mypage');
+    }
 }
+
