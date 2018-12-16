0168897027ebd796e69a40c0ea1d8ef7fbccc5f5
HiromichiYamamoto <gamon.co2@gmail.com>
2018-12-16 15:24:06 +0900

    モックを参考に入力フォームを追加

--- resources/views/front/post/create.blade.php
+++ resources/views/front/post/create.blade.php
@@ -4,6 +4,12 @@
 <div class="container">
     <div class="row">
         <div>記事登録画面</div>
+        <button>キャンセル</button>
+        <input type="text" name="url" placeholder="URL入力欄">
+        <input type="hidden" name="title" value="">
+        <input type="hidden" name="description" value="">
+        <textarea name="comment" id="" cols="30" rows="10" placeholder="コメント入力欄"></textarea>
+        <button type="submit">投稿する</button>
     </div>
 </div>
 @endsection
