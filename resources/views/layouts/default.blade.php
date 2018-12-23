038b918aedc10713518256711713784ed658e152
otaks <ohsofty@gmail.com>
2018-12-23 19:23:14 +0900

    不要なCSS設定削除

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,8 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link href="{{ asset('css/front.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
 </head>
 
 <body>
