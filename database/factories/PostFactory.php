9a20527de57927090251d9a7373ffbbfa47e0d89
wild7 <wild7_life25@yahoo.co.jp>
2018-12-24 12:10:54 +0900

    Postファクトリの調整 ・url属性のテスト値の見直し ・image属性の追加

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,9 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
-        'url'             => $faker->imageUrl(),
+        'url'             => $faker->url,
         'title'           => $faker->sentence,
         'description'     => $faker->paragraph,
         'is_picked_count' => 1,
+        'image'           => $faker->imageUrl(),
     ];
 });
