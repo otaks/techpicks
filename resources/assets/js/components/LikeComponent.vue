c5ce782dd0643e6788632abb026556be1da99952
otaks <ohsofty@gmail.com>
2019-01-02 12:46:21 +0900

    いいねボタン押下時の「Avoid mutating a prop directly since the value will be ～～」ワーニング対応

--- resources/assets/js/components/LikeComponent.vue
+++ resources/assets/js/components/LikeComponent.vue
@@ -3,11 +3,11 @@
 <template>
     <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">いいね数 {{goodNum}}</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">いいね数 {{this.goodNumInternal}}</button>
         </div>
         
         <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-primary" v-on:click="decrement(pickId, userId)" v-if="isLiked">いいね</button>
+            <button type="button" class="btn btn-sm btn-outline-primary" v-on:click="decrement(pickId, userId)" v-if="this.isLikedInternal">いいね</button>
 
             <button type="button" class="btn btn-sm btn-outline-secondary" v-on:click="increment(pickId, userId)" v-else>いいね</button>
         </div>
@@ -18,19 +18,25 @@
 export default {
     props:["goodNum", "pickId", "userId", "isLiked"],
     name:'like-component',
+    data:function(){
+        return{
+            goodNumInternal: this.goodNum,
+            isLikedInternal: this.isLiked
+        }
+    },
     methods:{
         increment: function($pid, $uid) {
             axios.get("increment/" + $pid  + "/" + $uid)
                 .then((r) => {
-                    this.goodNum = r.data.good_num
-                    this.isLiked = true
+                    this.goodNumInternal = r.data.good_num
+                    this.isLikedInternal = true
                 })
         },
         decrement: function($pid, $uid) {
             axios.get("decrement/" + $pid  + "/" + $uid)
                 .then((r) => {
-                    this.goodNum = r.data.good_num
-                    this.isLiked = false
+                    this.goodNumInternal = r.data.good_num
+                    this.isLikedInternal = false
                 })
         }
     }
