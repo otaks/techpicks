62769c0b1eaf726e060d575922ee26bf29b86c47
otaks <ohsofty@gmail.com>
2019-01-04 22:53:35 +0900

    変数名変更

--- resources/assets/js/components/Like.vue
+++ resources/assets/js/components/Like.vue
@@ -3,11 +3,11 @@
 <template>
     <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">いいね数 {{this.goodNumInternal}}</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">いいね数 {{this.isLikedCount}}</button>
         </div>
         
         <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-primary" v-on:click="decrement(post.id, user.id)" v-if="this.isLikedInternal">いいね</button>
+            <button type="button" class="btn btn-sm btn-outline-primary" v-on:click="decrement(post.id, user.id)" v-if="this.isLiked">いいね</button>
 
             <button type="button" class="btn btn-sm btn-outline-secondary" v-on:click="increment(post.id, user.id)" v-else>いいね</button>
         </div>
@@ -20,23 +20,23 @@ export default {
     name:'like-component',
     data:function(){
         return{
-            goodNumInternal: this.post.is_liked_count,
-            isLikedInternal: this.post.is_liked,
+            isLikedCount: this.post.is_liked_count,
+            isLiked: this.post.is_liked,
         }
     },
     methods:{
         increment: function($pid, $uid) {
             axios.get("increment/" + $pid  + "/" + $uid)
                 .then((r) => {
-                    this.goodNumInternal = r.data.good_num
-                    this.isLikedInternal = true
+                    this.isLikedCount = r.data.good_num
+                    this.isLiked = true
                 })
         },
         decrement: function($pid, $uid) {
             axios.get("decrement/" + $pid  + "/" + $uid)
                 .then((r) => {
-                    this.goodNumInternal = r.data.good_num
-                    this.isLikedInternal = false
+                    this.isLikedCount = r.data.good_num
+                    this.isLiked = false
                 })
         }
     }
