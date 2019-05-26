3e195da45c717d3fbe2b214c63178eb00ba54f13
otaks <ohsofty@gmail.com>
2019-05-26 20:07:47 +0900

    無限ローディング抜きで、ホームタブ表示実装中

---
+++ srv/db.go
@@ -0,0 +1,14 @@
+package tp
+
+import "time"
+
+type Post struct {
+	post_id     int
+	url         string
+	title       string
+	description string
+	pick_count  int
+	pv_count    int
+	created_at  time.Time
+	updated_at  time.Time
+}
